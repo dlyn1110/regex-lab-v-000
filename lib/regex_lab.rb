25bf271dd632f2882afe9050e168071c565c0ae9
Dani Lyn <dlyn1110@gmail.com>
2018-04-23 13:51:57 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,9 +15,9 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.gsub(/\w+/) { |w| w.capitalize + "!" }
+text.match(/^[A-Z].+\.?]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+phone.match(/^\(?[0-9]{3}/)
 end
